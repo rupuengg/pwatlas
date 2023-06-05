a569be5dc89052173bec9b6a2bb7c3a5a9ea4b13
Rupendra <rupu.engg@gmail.com>
2023-06-05 21:52:19 +0800

    Remove input props from App component Change in webpack config file for setting port and sochket

--- webpack/config.js
+++ webpack/config.js
@@ -5,7 +5,12 @@ module.exports = {
   entry: path.join(__dirname, '../src/main.tsx'),
   devtool: 'inline-source-map',
   devServer: {
-    static: './dist',
+    static: path.join(__dirname, '../dist'),
+    port: 3002,
+    compress: true,
+    client: {
+      webSocketUrl: 'ws://localhost:8080/ws'
+    }
   },
   module: {
     rules: [
@@ -38,7 +43,7 @@ module.exports = {
   target: ['web', 'es5'],
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: path.join(__dirname, '../public/index.html'),
     }),
   ],
 };
